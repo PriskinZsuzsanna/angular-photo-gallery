87417f7781d1d80e8ae0f4298db4706bd04bd1dc
Zsuzsi <priskin.zsuzsanna@gmail.com>
2023-05-24 22:37:08 +0200

    deploy

--- src/app/mock-beaches.ts
+++ src/app/mock-beaches.ts
@@ -3,37 +3,37 @@ import { Beach} from './Beach';
 export const BEACHES: Beach[] = [
   {
     id: 1,
-    text: 'Beach One',
+    text: 'Sunset',
     day: 'May 5th at 2:30pm',
     img: "../../assets/1.jpg"
   },
   {
     id: 2,
-    text: 'Beach Two',
+    text: 'Morning',
     day: 'May 6th at 1:30pm',
     img: "../../assets/2.jpg"
   },
   {
     id: 3,
-    text: 'Beach Three',
+    text: 'Twilight',
     day: 'May 7th at 12:30pm',
     img: "../../assets/3.jpg"
   },
   {
     id: 4,
-    text: 'Beach Four',
+    text: 'Morning Wsaves',
     day: 'May 5th at 2:30pm',
     img: "../../assets/4.jpg"
   },
   {
     id: 5,
-    text: 'Beach Five',
+    text: 'Peach',
     day: 'May 6th at 1:30pm',
     img: "../../assets/5.jpg"
   },
   {
     id: 6,
-    text: 'Beach Six',
+    text: 'Sandy beach',
     day: 'May 7th at 12:30pm',
     img: "../../assets/6.jpg"
   },
